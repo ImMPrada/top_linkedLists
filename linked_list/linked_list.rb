09ac0c0cf22e06f9d5ebea5d1f627c1f1be28714
immprada <im.mprada@gmail.com>
2022-10-20 21:59:02 -0500

    #size returns the total number of nodes in the list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -1,10 +1,13 @@
+require_relative 'node'
+
 module LinkedList
   class Single
-    attr_reader :head, :tail
+    attr_reader :head, :tail, :size
 
     def initialize
       @head = nil
       @tail = nil
+      @size = 0
     end
   end
 end
