17db65005d5eded06775a0674dc1b63891793508
immprada <im.mprada@gmail.com>
2022-10-20 21:59:23 -0500

    #append(value) adds a new node containing value to the end of the list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -9,5 +9,19 @@ module LinkedList
       @tail = nil
       @size = 0
     end
+
+    def append(value)
+      new_node = Node.new(value)
+      @size += 1
+
+      if @head.nil?
+        @head = new_node
+        @tail = new_node
+        @head.next_node = @tail
+      else
+        @tail.next_node = new_node
+        @tail = new_node
+      end
+    end
   end
 end
