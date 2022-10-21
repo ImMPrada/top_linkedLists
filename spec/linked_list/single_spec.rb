fce15fb744cfa616183e9fd56105b4591bc1935c
immprada <im.mprada@gmail.com>
2022-10-21 06:03:21 -0500

    add LinkedList::Single #prepend specs

--- spec/linked_list/single_spec.rb
+++ spec/linked_list/single_spec.rb
@@ -61,4 +61,49 @@ RSpec.describe LinkedList::Single do
       end
     end
   end
+
+  describe '#prepend' do
+    describe 'when the list is empty' do
+      before { list.prepend(1) }
+
+      it 'adds a node to the list' do
+        expect(list.head.value).to eq(1)
+      end
+
+      it 'sets the head and tail to the same node' do
+        expect(list.head).to eq(list.tail)
+      end
+
+      it 'increments the size' do
+        expect(list.size).to eq(1)
+      end
+
+      it 'sets tail next_node to nil' do
+        expect(list.tail.next_node).to be_nil
+      end
+    end
+
+    describe 'when the list is not empty' do
+      before do
+        list.append(1)
+        list.prepend(2)
+      end
+
+      it 'sets the head to the new node' do
+        expect(list.head.value).to eq(2)
+      end
+
+      it 'sets the head and tail to diferent nodes' do
+        expect(list.head).not_to eq(list.tail)
+      end
+
+      it 'increments the size' do
+        expect(list.size).to eq(2)
+      end
+
+      it 'sets tail next_node to nil' do
+        expect(list.tail.next_node).to be_nil
+      end
+    end
+  end
 end
