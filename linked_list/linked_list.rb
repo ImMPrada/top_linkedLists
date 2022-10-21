4de92e8fcfa5ea341eabd95736d5af94435e223f
immprada <im.mprada@gmail.com>
2022-10-20 22:01:47 -0500

    #prepend(value) adds a new node containing value to the start of the list

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -15,13 +15,32 @@ module LinkedList
       @size += 1
 
       if @head.nil?
-        @head = new_node
-        @tail = new_node
-        @head.next_node = @tail
+        first_add(new_node)
       else
         @tail.next_node = new_node
         @tail = new_node
       end
+    end
+
+    def prepend(value)
+      new_node = Node.new(value)
+      @size += 1
+
+      if @head.nil?
+        first_add(new_node)
+      else
+        old_head = @head
+        @head = new_node
+        @head.next_node = old_head
+      end
+    end
+
+    private
+
+    def first_add(new_node)
+      @head = new_node
+      @tail = new_node
+      @head.next_node = @tail
     end
   end
 end
